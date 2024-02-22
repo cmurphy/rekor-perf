6d0e6e34fc834493bc0aafeaf02efcc20d7e06ed
Colleen Murphy <colleenmurphy@google.com>
2024-02-22 15:23:50 -0800

    Install rekor-cli if needed

--- setup-rekor.sh
+++ setup-rekor.sh
@@ -3,6 +3,11 @@
 if [ ! -d rekor ] ; then
     git clone https://github.com/sigstore/rekor
 fi
+if ! which rekor-cli ; then
+    pushd rekor >/dev/null
+    go install ./cmd/rekor-cli
+    popd >/dev/null
+fi
 pushd rekor >/dev/null
 docker-compose up -d
 for i in `seq 1 6` ; do
