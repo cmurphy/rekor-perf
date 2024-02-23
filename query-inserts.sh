58759be73ee8d04cd774841c91bb5bb0ab83d682
Colleen Murphy <colleenmurphy@google.com>
2024-02-22 16:53:00 -0800

    Add retries for metrics scraping

--- query-inserts.sh
+++ query-inserts.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-sleep 5
+count=null
 
-count=$(curl -s http://localhost:9090/api/v1/query --data-urlencode 'query=rekor_index_storage_latency_summary_count{success="true"}' | jq -r .data.result[0].value[1])
+# may need to wait for the data to be scraped
+tries=0
+until [ "${count}" != "null" ] ; do
+    sleep 1
+    count=$(curl -s http://localhost:9090/api/v1/query --data-urlencode 'query=rekor_index_storage_latency_summary_count{success="true"}' | jq -r .data.result[0].value[1])
+    echo $count
+    let 'tries+=1'
+    if [ $tries -eq 6 ] ; then
+        echo "count query failed, here is the raw result:"
+        set -x
+        curl -s http://localhost:9090/api/v1/query --data-urlencode 'query=rekor_index_storage_latency_summary_count{success="true"}'
+        set +x
+        echo
+        exit 1
+    fi
+done
+
 avg=$(curl -s http://localhost:9090/api/v1/query --data-urlencode 'query=rekor_index_storage_latency_summary_sum{success="true"}/rekor_index_storage_latency_summary_count{success="true"}' | jq -r .data.result[0].value[1])
-
-if [ "${count}" == "null" ] ; then
-    echo "count query failed, here is the raw result:"
-    set -x
-    curl -s http://localhost:9090/api/v1/query --data-urlencode 'query=rekor_index_storage_latency_summary_count{success="true"}'
-    set +x
-    echo
-    exit 1
-fi
 
 if [ "${avg}" == "null" ] ; then
     echo "avg query failed, here is the raw result:"
